9b44a1339844079f14dfe2c79fe34ad3ebf891c9
Conrad Zelck <git@simpel.cc>
2022-04-14 17:39:30 +0200

    feat: introduce global head/header
    Signed-off-by: Conrad Zelck <git@simpel.cc>

---
+++ localSettingsHeader.php
@@ -0,0 +1,6 @@
+<?php
+  define("SITE_TITLE", "Comment Syntax");
+  define("SITE_COPYRIGHT_CONTENT", "MIT 2021 Simpel");
+  define("SITE_H1", "Comment Syntax");
+  define("SITE_GITHUB", "https://github.com/SimpelMe/comment-syntax-finder");
+?>
