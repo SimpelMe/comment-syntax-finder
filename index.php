40f219719553ff11fc506a79f4923ccd3cd8f4e9
Conrad Zelck <git@simpel.cc>
2024-02-27 23:08:19 +0100

    fix: use shared header styles
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- index.php
+++ index.php
@@ -1,53 +1,60 @@
 <html lang="en">
-  <head>
-    <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/head.php"; ?>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <header>
-      <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/header.php"; ?>
-    </header>
-    <main>
-      <div>
-        <label for="searchinput">Search language or comment syntax:</label>
-        <input type="search" id="searchinput" onkeyup="searchSyntax()" onsearch="searchSyntax()" title="Type in a scripting language or comments syntax" placeholder="search string …" autofocus>
-        <table id="resulttable">
-          <thead>
-            <tr>
-              <th title="programming, scripting or markdown language">language</th>
-              <th title="line (L) or multiline (M) comment">type</th>
-              <th>comment syntax</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-              $file = fopen("comments.csv", "r");
-              // Fetching data from csv file row by row
-              while (($data = fgetcsv($file)) !== false) {
-                // HTML tag for placing in row format
-                echo "<tr>";
-                foreach ($data as $i) {
-                  echo "<td>" . htmlspecialchars($i, ENT_QUOTES, 'UTF-8', false) . "</td>";
-                }
-                echo "</tr> \n";
-              }
-              fclose($file);
-            ?>
-          </tbody>
-        </table>
-      </div>
-    </main>
-    <footer>
-      <p>sources:</p>
-      <ul>
-        <li class="source"><a href="https://en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)" target="_blank">en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)</a></li>
-        <li class="source"><a href="http://rigaux.org/language-study/syntax-across-languages/Vrs.html" target="_blank">rigaux.org/language-study/syntax-across-languages/Vrs.html</a></li>
-        <li class="source"><a href="https://pldb.com/languages/line-comments-feature.html" target="_blank">pldb.com/languages/line-comments-feature.html</a></li>
-        <li class="source"><a href="https://pldb.com/languages/multiline-comments-feature.html" target="_blank">pldb.com/languages/multiline-comments-feature.html</a></li>
-      </ul>
-    </footer>
-  </body>
-  <script>
+
+<head>
+  <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/head.php"; ?>
+  <link rel="stylesheet" href="style.min.css">
+</head>
+
+<body>
+  <header>
+    <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/header.php"; ?>
+  </header>
+  <main>
+    <div>
+      <label for="searchinput">Search language or comment syntax:</label>
+      <input type="search" id="searchinput" onkeyup="searchSyntax()" onsearch="searchSyntax()"
+        title="Type in a scripting language or comments syntax" placeholder="search string …" autofocus>
+      <table id="resulttable">
+        <thead>
+          <tr>
+            <th title="programming, scripting or markdown language">language</th>
+            <th title="line (L) or multiline (M) comment">type</th>
+            <th>comment syntax</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $file = fopen("comments.csv", "r");
+          // Fetching data from csv file row by row
+          while (($data = fgetcsv($file)) !== false) {
+            // HTML tag for placing in row format
+            echo "<tr>";
+            foreach ($data as $i) {
+              echo "<td>" . htmlspecialchars($i, ENT_QUOTES, 'UTF-8', false) . "</td>";
+            }
+            echo "</tr> \n";
+          }
+          fclose($file);
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </main>
+  <footer>
+    <p>sources:</p>
+    <ul>
+      <li class="source"><a href="https://en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)"
+          target="_blank">en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)</a></li>
+      <li class="source"><a href="http://rigaux.org/language-study/syntax-across-languages/Vrs.html"
+          target="_blank">rigaux.org/language-study/syntax-across-languages/Vrs.html</a></li>
+      <li class="source"><a href="https://pldb.com/languages/line-comments-feature.html"
+          target="_blank">pldb.com/languages/line-comments-feature.html</a></li>
+      <li class="source"><a href="https://pldb.com/languages/multiline-comments-feature.html"
+          target="_blank">pldb.com/languages/multiline-comments-feature.html</a></li>
+    </ul>
+  </footer>
+</body>
+<script>
   function searchSyntax() {
     var input, filter, table, tr, tdOne, tdThree, i, txtValue;
     input = document.getElementById("searchinput");
@@ -68,5 +75,6 @@
       }
     }
   }
-  </script>
+</script>
+
 </html>
